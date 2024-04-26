e551024c4c59af53adb71f98191a550e2a557dda
Dan Brady <daniel.brady@sheffield.ac.uk>
2024-04-26 11:37:22 +0100

    Add redis as session host

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_session import Session
 import redis
 import os
 from rq import Queue
@@ -11,5 +12,13 @@ conn = redis.from_url(redis_url, password=os.getenv("REDIS_PASSWORD", ""))
 
 # Connect to redis queue
 redQueue = Queue(connection=conn)
+
+# Set up storing session variables in redis
+# Uses as csv files are typically larger than usual for client side session data
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False # Current session will be lost on exit
+app.config['SESSION_REDIS'] = conn
+
+server_session = Session(app)
 
 from app import routes
